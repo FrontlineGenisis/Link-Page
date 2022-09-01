eb387e0f4dec1a48e262eef36ab3bd2c8e86fce1
Marshall Woods <80414685+FrontlineGenisis@users.noreply.github.com>
2022-09-01 11:33:59 +0000

    added php auto optimizer script

---
+++ optimize.php
@@ -0,0 +1,19 @@
+<?php
+function sanitize_output($buffer) {
+$search = array(
+'/\>[^\S ]+/s',     
+'/[^\S ]+\</s',     
+'/(\s)+/s',        
+'/<!--(.|\s)*?-->/'
+);
+$replace = array(
+'>',
+'<',
+'\\1',
+''
+);
+$buffer = preg_replace($search, $replace, $buffer);
+return $buffer;
+}
+ob_start("sanitize_output");
+?>
